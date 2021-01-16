06bcaf3cdc04e3b45912f446d8810c5486003f56
unknown <srinivas.chakarvarthy@gmail.com>
2021-01-16 16:35:02 +0530

    styling due for product detail page

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../css/Details.css'
 
 
 function Details(props) {
+    const [currentImage, setImage] = useState(props.location.state.url);
+    
+    function handleHover (event) {
+        setImage(event.target.src);
+    }
+
     return (
         <div className="detailsContainer">
            <div className="imageSection">
                 {props.location.state.images!=null ? props.location.state.images.map((item) => {
-                    return <img src={item} className="imageSmall" />
+                    return <img src={item} onMouseEnter={handleHover} className="imageSmall" />
                 }) : <div></div>}
             </div>
             <div className="image">
-                <img src={props.location.state.url} className="imageBig" />
+                <img src={currentImage} className="imageBig" />
             </div>
             <div className="info">
                 <div className="productTitle">
@@ -23,22 +29,22 @@ function Details(props) {
                    <p>Order within 12 hrs and 59 mins</p>
                    <p><strong>EMI</strong> starts at ₹324. No Cost EMI available </p>
                     <div className="iconRow">
-                        <div className="icon">
+                        <div className="iconItem">
                             <img src="https://images-na.ssl-images-amazon.com/images/G/31/A2I-Convert/mobile/IconFarm/icon-cod._CB485937110_.png" />
                             <span>Pay</span>
                             <span>on Delivery</span>
                         </div>
-                        <div className="icon">
+                        <div className="iconItem">
                             <img src="https://images-na.ssl-images-amazon.com/images/G/31/A2I-Convert/mobile/IconFarm/icon-returns._CB484059092_.png" />
                             <span>& Day</span>
                             <span>Replacement</span>
                         </div>
-                        <div className="icon">
+                        <div className="iconItem">
                             <img src="https://images-na.ssl-images-amazon.com/images/G/31/A2I-Convert/mobile/IconFarm/icon-amazon-delivered._CB485933725_.png" />
                             <span>Amazon</span>
                             <span>Delivered</span>
                         </div>
-                        <div className="icon">
+                        <div className="iconItem">
                             <img src="https://images-na.ssl-images-amazon.com/images/G/31/A2I-Convert/mobile/IconFarm/icon-warranty._CB485935626_.png" />
                             <span>1 year</span>
                             <span>Warranty</span>
