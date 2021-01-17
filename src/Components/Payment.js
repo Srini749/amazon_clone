11ec38e3e9edd35162cd6d892a1f7949da841c8c
unknown <srinivas.chakarvarthy@gmail.com>
2021-01-17 14:28:23 +0530

    payment page-review items added

--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../css/Payment.css'
 import {useStateValue} from '../StateProvider'
-
+import CheckoutProduct from './CheckoutProduct'
 
 function Payment() {
     const [state, dispatch] = useStateValue();
@@ -12,34 +12,40 @@ function Payment() {
                     Checkout (
                         {state.basket.length} items)
                 </h1>
-                <div className='payment__section'>
-                    <div className='payment__title'>
-                        <h3>Delivery Address</h3>
+                <div className='payment_section'>
+                    <div className='payment_title'>
+                        <h5><strong>Delivery Address</strong></h5>
                     </div>
-                    <div className='payment__address'>
+                    <div className='payment_address'>
                         <p>{state.user?.email}</p>
                         <p>123 React Lane</p>
                         <p>Los Angeles, CA</p>
                     </div>
                 </div>
 
-                <div className='payment__section'>
-                    <div className='payment__title'>
-                        <h3>Review items and delivery</h3>
+                <div className='payment_section'>
+                    <div className='payment_title'>
+                        <h5><strong>Review items and delivery</strong></h5>
                     </div>
-                    <div className='payment__items'>
-                        {/* {state.basket.map(item => (
+                    <div className='payment_items'>
+                        {state.basket.map(item => (
                             <CheckoutProduct
-                                id={item.id}
                                 title={item.title}
-                                image={item.image}
+                                url={item.url}
                                 price={item.price}
                                 rating={item.rating}
                             />
-                        ))} */}
+                        ))}
                     </div>
                 </div>
 
+                <div className='payment_section'>
+                    <div className="payment_title">
+                        <h5><strong>Payment Method</strong></h5>
+                    </div>
+                    <div className="payment_details">
+                    </div>
+                </div>
 
             </div>
         </div>
